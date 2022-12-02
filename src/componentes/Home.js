9e158d7b17c632063340b801b87d3c330ffd73c0
Michel Machin <michelma1988@gmail.com>
2022-12-02 11:38:46 -0500

    cambio tamaño de letra del homepage

--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -9,16 +9,13 @@ export default function Home()
 {    
     return (       
         <div className='container-fluid d-flex flex-column justify-content-center  position-relative align-items-center h-100 overflow-auto'>                                
-           <div className=" d-flex m-4  justify-content-center p-5 bg-light shadow bg-opacity-50 ">
-            <h1 className=" text-center gradient_text fw-bold border-bottom" style={{fontSize:('7vw')}}>Lee</h1>
-            <h1 className=" text-center text-dark border-top" style={{fontSize:('7vw')}}>NAILS</h1> 
+           <div className=" d-flex m-4  justify-content-center p-5 bg-light rounded shadow bg-opacity-50 ">
+            <h1 className=" text-center gradient_text fw-bold border-bottom"  style={{fontSize:('9vw')}}>Lee</h1>
+            <h1 className=" text-center text-dark border-top"  style={{fontSize:('9vw')}}>NAILS</h1> 
            </div>
-           <div className="shadow m-4  bg-primary rounded " > 
-           
-            <Link to='/Catalogo'> <button className="btn btn-primary w-100" id="solid" ><Book  className="me-2"/>Ver Catalogo</button></Link>
-           
+           <div className="shadow m-4  bg-primary rounded " >            
+            <Link to='/Catalogo'> <button className="btn btn-primary w-100" id="solid" ><Book  className="me-2"/>Ver Catalogo</button></Link>           
            </div> 
-        </div>  
-                   
+        </div>                     
     );
 }
